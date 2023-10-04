86af20adad1f8305a7589a7942f2720373cd8094
asingh80 <aarushisingh5@gmail.com>
2023-10-03 21:52:36 -0400

    Some tweaks to MinMaxCalculation.java

--- MinMaxCalculation.java
+++ MinMaxCalculation.java
@@ -1,22 +1,23 @@
 public class MinMaxCalculation {
     public static void main(String[] args) {
-        int[] set = {5, 12, 9, 7, 3, 15, 6}; 
+        
+        double[] set = {5.7, 12.3, 9, 7.999, 3, 15.02, 15.2, 15.200, 6, 0.87}; 
 
-        int[] minMax = findMinMax(set);
+        double[] minMax = findMinMax(set);
 
         System.out.println("Min: " + minMax[0]);
         System.out.println("Max: " + minMax[1]);
     }
 
-    public static int[] findMinMax(int[] set) {
+    public static double[] findMinMax(double[] set) {
 
         if (set.length == 0) { // when array is empty:
             System.out.println("The array is empty.");
-            return new int[] {0, 0}; 
+            return new double[] {0, 0}; 
         }
 
-        int min = set[0]; 
-        int max = set[0]; 
+        double min = set[0]; 
+        double max = set[0]; 
 
         for (int i = 1; i < set.length; i++) {
             if (set[i] < min) {
@@ -28,6 +29,6 @@ public class MinMaxCalculation {
             }
         }
 
-        return new int[] {min, max};
+        return new double[] {min, max};
     }
 }
